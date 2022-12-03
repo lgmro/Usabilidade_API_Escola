9d8104eaad3be2ae95f67df0a9ef9afabaea8b2f
marileite96 <mfernandesleite0@gmail.com>
2022-12-03 17:46:42 -0300

    feat: criando rotas alunos

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import alunos from "./controllers/Alunos.js";
 import professores from "./controllers/Professores.js";
 import disciplina from "./controllers/Disciplina.js";
 import express from "express";
@@ -7,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(professores, disciplina);
+app.use(professores, disciplina, alunos);
 
 //Quando não encontrar alguma rota
 app.use((req,res,next) => {
---
+++ src/controllers/Alunos.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { selecionarAlunos, selecionarAluno, cadastrarAluno, atualizarDadosAluno, deletarAluno } from "../models/Alunos.js";
+
+const Alunos_routes = Router();
+
+Alunos_routes.get("/Alunos", selecionarAlunos);
+Alunos_routes.get("/Alunos/:id", selecionarAluno);
+Alunos_routes.post("/Alunos", cadastrarAluno);
+Alunos_routes.put("/Alunos", atualizarDadosAluno);
+Alunos_routes.delete("/Alunos/:id", deletarAluno);
+
+export default Alunos_routes;
